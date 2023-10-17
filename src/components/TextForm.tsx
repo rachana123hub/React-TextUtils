980beff2668bd48c047769312ecadb392a872c9d
Rachana Wankar <rachana.wankar@nihilent.com>
2023-10-17 16:10:04 +0530

    build updated

--- src/components/TextForm.tsx
+++ src/components/TextForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props: any) {
-    const convertToUpper = (action: string) => {
+    const manuculateText = (action: string) => {
         let newText = '';
         switch (action) {
             case 'upper':
@@ -25,6 +25,7 @@ export default function TextForm(props: any) {
                 copyText.select();
                 newText = copyText.value;
                 navigator.clipboard.writeText(copyText.value);
+                document.getSelection().removeAllRanges();
                 props.showAlert('Text copied');
                 break;
             case 'removeExtraSpace':
@@ -44,16 +45,16 @@ export default function TextForm(props: any) {
             <div className="container mb-3">
                 <h1><label htmlFor="exampleFormControlTextarea1" className="form-label">Enter your text</label></h1>
                 <textarea style={props.mode === 'dark' ? { backgroundColor: '#1d16167d', color: 'white' } : { backgroundColor: 'white', color: 'black' }} className="form-control my-2" id="exampleFormControlTextarea1" rows={8} value={text} onChange={handleOnChange}></textarea>
-                <button className="btn btn-primary mx-2" onClick={(event) => convertToUpper('upper')}>Covert to uppercase</button>
-                <button className="btn btn-warning mx-2" onClick={(event) => convertToUpper('lower')}>Covert to lowercase</button>
-                <button className="btn btn-secondary mx-2" onClick={(event) => convertToUpper('removeSpace')}>Remove spaces</button>
-                <button className="btn btn-info mx-2" onClick={(event) => convertToUpper('copy')}>Copy Text</button>
-                <button className="btn btn-dark mx-2" onClick={(event) => convertToUpper('removeExtraSpace')}>Remove Extra sapce</button>
-                <button className="btn btn-danger mx-2" onClick={(event) => convertToUpper('clear')}>Clear Text</button>
+                <button className="btn btn-primary mx-2" onClick={(event) => manuculateText('upper')} disabled={!text}>Covert to uppercase</button>
+                <button className="btn btn-warning mx-2" onClick={(event) => manuculateText('lower')} disabled={!text}>Covert to lowercase</button>
+                <button className="btn btn-secondary mx-2" onClick={(event) => manuculateText('removeSpace')} disabled={!text}>Remove spaces</button>
+                <button className="btn btn-info mx-2" onClick={(event) => manuculateText('copy')} disabled={!text}>Copy Text</button>
+                <button className="btn btn-dark mx-2" onClick={(event) => manuculateText('removeExtraSpace')} disabled={!text}>Remove Extra sapce</button>
+                <button className="btn btn-danger mx-2" onClick={(event) => manuculateText('clear')} disabled={!text}>Clear Text</button>
             </div>
             <div className="container">
                 <h2>Your text summery</h2>
-                <p>{text ? text.trim().split(' ').length : 0} words {text.length} characters</p>
+                <p>{text ? text.trim().split(' ').filter((t) => t.length !== 0).length : 0} words {text.length} characters</p>
                 <p>{text ? 0.008 * text.trim().split(' ').length : 0} minutes read</p>
                 <h2>Preview</h2>
                 <pre>{!text ? 'Enter the text in above text box to preview' : text}</pre>
